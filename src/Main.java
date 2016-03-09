4df74b1eb3cd3964bd98a9595b88b2f0ec25736b
duansishu <dss886@gmail.com>
2016-03-09 18:09:03 +0800

    add solutions

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-import solutions.easy.No013;
+import solutions.easy.No070;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(new No013().romanToInt("LXXX"));
+        System.out.println(new No070().climbStairs(44));
     }
 }
