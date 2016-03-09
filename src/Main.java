fe815c14053599a415509cd170063f55a69eb53f
duansishu <dss886@gmail.com>
2016-03-09 16:57:28 +0800

    addadd solutions

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import solutions.easy.No117;
-import solutions.easy.No169;
+import solutions.easy.No013;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] x = {2, 2, 1, 1, 1, 2, 2};
-        System.out.println(new No169().majorityElement(x));
+        System.out.println(new No013().romanToInt("LXXX"));
     }
 }
---
+++ src/solutions/easy/No206.java
@@ -0,0 +1,31 @@
+package solutions.easy;
+
+import util.ListNode;
+
+/**
+ * Created by dss886 on 2016/3/9.
+ * https://leetcode.com/problems/reverse-linked-list/
+ *
+ * code can be more elegant:
+ * https://leetcode.com/discuss/34474/in-place-iterative-and-recursive-java-solution
+ */
+public class No206 {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode p = head.next;
+        head.next = null;
+        while (p != null) {
+            ListNode temp = p.next;
+            p.next = head;
+            head = p;
+            p = temp;
+        }
+        return head;
+    }
+
+    private void ete() {
+
+    }
+}
