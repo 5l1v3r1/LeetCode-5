bd2f41e34b375c99e46e3929fc149d4a23be4b6b
duansishu <dss886@gmail.com>
2016-03-12 16:59:10 +0800

    add solutions

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-import solutions.easy.No202;
+import solutions.easy.No191;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(new No202().isHappy(20));
+        System.out.println(new No191().hammingWeight(24));
     }
 }
