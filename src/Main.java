cd7fa0e33bd3a02bfd621c913aebd1bcf87351e2
duansishu <dss886@gmail.com>
2016-03-16 17:45:36 +0800

    add solutions

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,20 @@ import util.*;
 public class Main {
 
     public static void main(String[] args) {
-
-//        int[] list = new int[]{-1, -1};
+//        int[] list = new int[]{-8261, 2300, -1429, 6274, 9650, -3267, 1414, -8102, 6251, -5979, -5291, -4616, -4703};
 //        int[] list2 = new int[]{2, 4, 6, 8, 10};
 
 //        new No088().merge(list1, 5, list2, 5);
 //        System.out.println(list1);
 
-        System.out.println(new No290().wordPattern("abba", "dog cat cat dog"));
+//        System.out.println(new No067().addBinary("11", "10"));
 
 //        TreeNode node = new TreeNode(1);
 //        new No107().levelOrderBottom(node);
 
+//        ListNode head = new ListNode(1);
+//        head.next = new ListNode(2);
+        System.out.println(new No028().strStr("mississippi", "issipi"));
+//        System.out.println("123".indexOf(""));
     }
 }
