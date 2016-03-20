49ad476d90ea2bf846c146116b9539628100ff81
dss886 <dss886@gmail.com>
2016-03-20 22:43:41 +0800

    add solutions

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-import solutions.easy.*;
+import solutions.medium.*;
 import util.*;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Main {
 
@@ -10,7 +11,8 @@ public class Main {
 //        int[] list2 = new int[]{2, 4, 6, 8, 10};
 //        new No189().rotate(list, 3);
 //        System.out.println(Arrays.toString(list));
-        System.out.println(new No008().myAtoi("123  456"));
-
+        System.out.println(Arrays.toString(new No338().countBits(10)));
+//        System.out.println(new No008().myAtoi("123  456"));
+//        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
     }
 }
