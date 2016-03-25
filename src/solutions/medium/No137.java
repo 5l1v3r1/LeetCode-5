b683787c09608135da620e43ba99294eb965d35c
duansishu <dss886@gmail.com>
2016-03-25 17:15:33 +0800

    add solutions

---
+++ src/solutions/medium/No137.java
@@ -0,0 +1,14 @@
+package solutions.medium;
+
+/**
+ * Created by dss886 on 2016/3/25.
+ * https://leetcode.com/problems/single-number-ii/
+ *
+ * The operator 'xor' can be seen as 'binary add without carry',
+ * so we can create an loop for this add operation.
+ */
+public class No137 {
+    public int singleNumber(int[] nums) {
+
+    }
+}
